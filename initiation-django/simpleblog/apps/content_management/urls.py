8beb7dcdf6b918be3d60811424d37860e47d4056
TonyEight <ludovic.legendart@gmail.com>
2018-05-24 17:28:46 +0200

    Ship first version :metal:

--- initiation-django/simpleblog/apps/content_management/urls.py
+++ initiation-django/simpleblog/apps/content_management/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'content_management'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path(
+        'category/<int:pk>/',
+        views.CategoryDetailView.as_view(),
+        name='category-detail'
+    ),
+    path(
+        'article/<int:pk>/',
+        views.ArticleRedirectView.as_view()
+    ),
+    path(
+        'article/<slug:slug>/',
+        views.ArticleDetailView.as_view(),
+        name='article-detail'
+    ),
 ]
--- initiation-django/simpleblog/apps/content_management/views.py
+++ initiation-django/simpleblog/apps/content_management/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views import generic
 from .models import Category, Article
 from django.db.models import Count
@@ -11,3 +12,50 @@ class HomeView(generic.ListView):
         return Category.objects.annotate(
             article_count=Count('articles')
         ).filter(article_count__gt=0)
+
+
+class CategoryDetailView(generic.DetailView):
+    template_name='content_management/category_detail.html'
+    context_object_name='category'
+    model = Category
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context.update({
+            'categories': Category.objects.annotate(
+                article_count=Count('articles')
+            ).filter(article_count__gt=0)
+        })
+        return context
+
+
+class ArticleRedirectView(generic.base.RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'content_management:article-detail'
+
+    def get_redirect_url(self, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        return super().get_redirect_url(article.slug)
+
+class ArticleDetailView(generic.DetailView):
+    template_name='content_management/article_detail.html'
+    context_object_name='article'
+    model = Article
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        current_category_pk = self.request.GET.get('category', None)
+        current_category = None
+        related_articles = None
+        if current_category_pk is not None:
+            current_category = Category.objects.get(pk=int(current_category_pk))
+            related_articles = current_category.articles.exclude(pk=self.get_object().pk)
+        context.update({
+            'categories': Category.objects.annotate(
+                article_count=Count('articles')
+            ).filter(article_count__gt=0),
+            'current_category': current_category,
+            'related_articles': related_articles
+        })
+        return context
